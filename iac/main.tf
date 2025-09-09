3ee29c133300bf56e9ae1e63d34679737e44a0a2
uday gowda <udayagowdabstvk@gmail.com>
2025-09-09 19:08:02 +0530

    Initial commit - AWS CI/CD Demo

---
+++ iac/main.tf
@@ -0,0 +1,9 @@
+# Example Terraform file
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "app_bucket" {
+  bucket = "cicd-demo-app-bucket-example"
+  acl    = "public-read"
+}
